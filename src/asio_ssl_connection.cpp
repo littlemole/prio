b7807a4aaf845e13385496f60b18bfd7fdbe4362
little.mole <little.mole@oha7.org>
2018-03-11 20:44:50 +0100

    ssl boost_asio timeouts

--- src/asio_ssl_connection.cpp
+++ src/asio_ssl_connection.cpp
@@ -9,11 +9,9 @@
 #ifdef _WIN32
 #include <inttypes.h>
 typedef int ssize_t;
-//#include <nghttp2/nghttp2.h>
 #define close_socket ::closesocket
 #define SHUT_WR SD_SEND 
 #else
-//#include <nghttp2/nghttp2.h>
 #define close_socket ::close
 #endif
 
@@ -152,15 +150,14 @@ Future<Connection::Ptr, std::string> SslConnection::read()
 	auto p = promise<Connection::Ptr,std::string>();
 	auto ptr = shared_from_this();
 
-	//cancel();
-/*
+
 	impl_->timer.after(timeouts_.rw_timeout_s)
 	.then( [this,p]()
 	{
 		impl_->socket.lowest_layer().cancel();
 		p.reject(IoTimeout("ssl read cancelled due to timeout"));
 	});	
-*/
+
 	impl_->socket.async_read_some(
 		boost::asio::buffer(impl_->data,impl_->max_length),
 		[this,ptr,p](const boost::system::error_code& error,std::size_t bytes_transferred)
@@ -200,15 +197,13 @@ Future<Connection::Ptr, std::string> SslConnection::read(size_t s)
 	auto p = promise<Connection::Ptr,std::string>();
 	auto ptr = shared_from_this();
 
-	//cancel();
-/*
 	impl_->timer.after(timeouts_.rw_timeout_s)
 	.then( [this,p]()
 	{
 		impl_->socket.lowest_layer().cancel();
 		p.reject(IoTimeout("ssl read(n) cancelled due to timeout"));
 	});	
-*/
+
 	std::shared_ptr<std::vector<char>> buffer = std::make_shared<std::vector<char>>(s,0);
 
 	async_read(
@@ -244,16 +239,12 @@ Future<Connection::Ptr> SslConnection::write( const std::string& data)
 	auto p = promise<Connection::Ptr>();
 	auto ptr = shared_from_this();
 
-	//cancel();
-
-	/*
 	impl_->timer.after(timeouts_.rw_timeout_s)
 	.then( [this,p]()
 	{
 		impl_->socket.lowest_layer().cancel();
 		p.reject(IoTimeout("ssl write cancelled due to timeout"));
 	});	
-*/
 
 	std::shared_ptr<std::string> buffer = std::make_shared<std::string>(data);
 
@@ -288,7 +279,6 @@ Future<Connection::Ptr> SslConnection::write( const std::string& data)
 void SslConnection::close()
 {
 	std::cout << "SslCOnnection::close()" << std::endl;
-	//cancel();
 	if(impl_->socket.lowest_layer().is_open())
 	{
 		impl_->socket.lowest_layer().close();
@@ -355,13 +345,6 @@ SslCtxImpl::SslCtxImpl()
 SslCtx::SslCtx()
 	: ctx(new SslCtxImpl)
 {
-	/*
-	ctx->ssl.set_options(
-        boost::asio::ssl::context::default_workarounds
-        | boost::asio::ssl::context::no_sslv2
-	);
-	*/
-	
 	EC_KEY *ecdh = 0;
 	SSL_CTX* sslCtx = ctx->ssl.native_handle();
 	SSL_CTX_set_options(
@@ -392,64 +375,6 @@ void SslCtx::load_cert_pem(const std::string& file)
 	ctx->ssl.use_certificate_chain_file(file);
 	ctx->ssl.use_private_key_file(file, boost::asio::ssl::context::pem);
 }
-/*
-int next_proto_cb(SSL *ssl, const unsigned char **data,unsigned int *len, void *arg);
-
-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
-int alpn_select_proto_cb(SSL *ssl, const unsigned char **out,
-          unsigned char *outlen, const unsigned char *in,
-		  unsigned int inlen, void *arg);
-#endif
-
-
-static int select_next_proto_cb(SSL *ssl, unsigned char **out,
-	unsigned char *outlen, const unsigned char *in,
-	unsigned int inlen, void *arg) 
-{
-	if (nghttp2_select_next_protocol(out, outlen, in, inlen) <= 0) 
-	{
-		std::cout << "err select_next_proto_cb ACK failed -no http2" << std::endl;
-		return SSL_TLSEXT_ERR_NOACK;
-	}
-	return SSL_TLSEXT_ERR_OK;
-}
-
-void SslCtx::enableHttp2()
-{
-	SSL_CTX* sslCtx = ctx->ssl.native_handle();
-
-	next_proto_list[0] = NGHTTP2_PROTO_VERSION_ID_LEN;
-	memcpy(&next_proto_list[1], NGHTTP2_PROTO_VERSION_ID,
-	NGHTTP2_PROTO_VERSION_ID_LEN);
-	next_proto_list_len = 1 + NGHTTP2_PROTO_VERSION_ID_LEN;
-	
-	SSL_CTX_set_next_protos_advertised_cb(sslCtx, next_proto_cb, NULL);
-	
-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
-	SSL_CTX_set_alpn_select_cb(sslCtx, alpn_select_proto_cb, NULL);
-#endif // OPENSSL_VERSION_NUMBER >= 0x10002000L
-}
-
-void SslCtx::enableHttp2Client(  )
-{
-	ctx->enableHttp2Client();
-}
-
-void SslCtxImpl::enableHttp2Client(  )
-{
-	SSL_CTX* sslCtx = ssl.native_handle();
-	next_proto_list[0] = NGHTTP2_PROTO_VERSION_ID_LEN;
-	memcpy(&next_proto_list[1], NGHTTP2_PROTO_VERSION_ID,
-	NGHTTP2_PROTO_VERSION_ID_LEN);
-	next_proto_list_len = 1 + NGHTTP2_PROTO_VERSION_ID_LEN;
-
-	SSL_CTX_set_next_proto_select_cb(sslCtx, select_next_proto_cb, NULL);
-	
-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
-	  SSL_CTX_set_alpn_protos(sslCtx, (const unsigned char *)"\x02h2", 3);
-#endif // OPENSSL_VERSION_NUMBER >= 0x10002000L	  
-}
-*/
 
 } // close namespaces
 
