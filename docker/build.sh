c19d00d5ce16717846d40480af41a7bf48bd7ae3
littlemole <little.mole@oha7.org>
2018-04-19 21:20:40 +0200

    cmake fun

--- docker/build.sh
+++ docker/build.sh
@@ -6,6 +6,33 @@ echo "building $1 for $BACKEND with"
 echo "$CXX using $BUILDCHAIN"
 echo "**********************************"
 
+function cmake_build {
+
+    MODE="$1"
+    
+    mkdir -p $MODE
+    cd $MODE
+
+    if [ "$BACKEND" == "libevent" ]
+    then
+        cmake .. -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=$MODE
+    else
+        cmake .. -DCMAKE_CXX_COMPILER=$CXX -DWITH_LIBEVENT=Off -DCMAKE_BUILD_TYPE=$MODE
+    fi
+     
+    make
+    
+    if [ "$SKIPTEST" == "true" ]
+    then
+    	echo "skipping tests for $1 ..."
+    else
+        ctest
+    fi
+    
+    make install  
+    cd ..
+}
+
 cd /usr/local/src/$1
 
 if [ "$BUILDCHAIN" == "make" ] 
@@ -20,17 +47,8 @@ then
     make clean
     make -e install
 else
-    mkdir -p build
-    cd build
 
-    if [ "$BACKEND" == "libevent" ]
-    then
-        cmake .. -DCMAKE_CXX_COMPILER=$CXX
-    else
-        cmake .. -DCMAKE_CXX_COMPILER=$CXX -DWITH_LIBEVENT=Off
-    fi 
-    make
-    make test
-    make install
+    cmake_build "Debug"
+    cmake_build "Release"
 fi
 
