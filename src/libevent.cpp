5db059da986d33fa20e32d5cf49b77a814441742
little.mole <little.mole@oha7.org>
2018-09-06 20:18:31 +0200

    cleanup

--- src/libevent.cpp
+++ src/libevent.cpp
@@ -589,7 +589,6 @@ Future<socket_t> Resolver::connect(const std::string& host, int port)
 
 		for (rp = result; rp != NULL; rp = rp->ai_next) 
 		{
-//			fprintf(stderr,"next:\r\n");
 			struct sockaddr_in *sin = (struct sockaddr_in *)(rp->ai_addr);
 
 // to show DNS resolves:
